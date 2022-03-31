72f41713b4b5799992c9060207bdaf1ec5f4a0f3
Finoana71 <77326289+Finoana71@users.noreply.github.com>
2022-03-31 21:54:12 +0300

    ajouter morgan

--- server.js
+++ server.js
@@ -28,16 +28,16 @@ const expressJwt = require('express-jwt');
 //   }
   
     // Make sure you place body-parser before your CRUD handlers
-    app.use(bodyParser.urlencoded({extended: true}))
+    //app.use(bodyParser.urlencoded({extended: true}))
 
     // Pour que le serveur accepte des datas JSON 
-    app.use(bodyParser.json())
+    //app.use(bodyParser.json())
 
-    app.use(logger('dev'));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    app.use(cookieParser());
-    app.use(express.static(path.join(__dirname, 'public')));
+    //app.use(logger('dev'));
+    //app.use(express.json());
+    //app.use(express.urlencoded({ extended: false }));
+    //app.use(cookieParser());
+    //app.use(express.static(path.join(__dirname, 'public')));
 
     // // view engine setup
     // app.set('views', path.join(__dirname, 'views'));
@@ -45,33 +45,33 @@ const expressJwt = require('express-jwt');
 
 
     // // Ajouter des routeurs
-     var indexRouter = require('./backend/src/routes/index');
+    // var indexRouter = require('./src/routes/index');
     // var usersRouter = require('./src/routes/users');
 
     // // Utiliser les routeurs
-     app.use('/api/', indexRouter);
+     //app.use('/api/', indexRouter);
     // app.use('/api/utilisateurs', usersRouter);
 
-    let pathNoToken = ['/api/utilisateurs/connexion', '/api/utilisateurs/inscription']
-    app.use(expressJwt({secret: 'ekalySecret', algorithms: ["RS256"]}).unless({path: pathNoToken}));
+    //let pathNoToken = ['/api/utilisateurs/connexion', '/api/utilisateurs/inscription']
+    //app.use(expressJwt({secret: 'ekalySecret', algorithms: ["RS256"]}).unless({path: pathNoToken}));
 
     // catch 404 and forward to error handler
-    app.use(function(req, res, next) {
-    next(createError(404));
-    });
+    //app.use(function(req, res, next) {
+    //next(createError(404));
+    //});
 
     // error handler
-    app.use(function(err, req, res, next) {
-        // set locals, only providing error in development
-        res.locals.message = err.message;
-        res.locals.error = req.app.get('env') === 'development' ? err : {};
+    //app.use(function(err, req, res, next) {
+         //set locals, only providing error in development
+        //res.locals.message = err.message;
+        //res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-        // render the error page
-        res.status(err.status || 500);
-        res.send({message: err.message})
-    });
+         //render the error page
+        //res.status(err.status || 500);
+        //res.send({message: err.message})
+    //});
 
-    app.use(express.static('public'));
+    //app.use(express.static('public'));
 
   // start the Express server
 // });
